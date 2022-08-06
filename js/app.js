1cd676b7e360943de2b3968afb812588bc7c0897
pekaredestroyer <109276834+pekaredestroyer@users.noreply.github.com>
2022-08-07 00:33:05 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let number1 = 4;
+let number2 = 2;
+
+let summery = number1 + number2;
+let minus = number1 - number2;
+let multiply = number1 * number2;
+let devide = number1/number2;
+
+
+
+console.log("это: " + "4 + 2 = " + summery);
+console.log("было: " + "4 - 2 = " + minus);
+console.log("сложно: " + "4 * 2 =" + multiply);
+console.log("спасибо Сандрине!)) " + "4 / 2 =" + devide);
